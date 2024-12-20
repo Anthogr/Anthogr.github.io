dbb4419cccb382af59d6a2ac39f846c446e9a787
Anthogr <87010226+Anthogr@users.noreply.github.com>
2024-12-20 18:17:45 +0100

    end tutorial Make a content collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,17 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection("blog");
   return rss({
     title: 'Hello boy',
     description: 'This is how I learn stuff',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>fr</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+    })),    customData: `<language>fr</language>`,
   });
 }
